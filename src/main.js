77c12df4584b78cbd57a6722e1a68964ba28a738
Dmytro Koval <kdmytro793@gmail.com>
2024-11-02 16:26:41 +0200

    hw11

---
+++ src/main.js
@@ -0,0 +1,54 @@
+import fetchData from "./js/pixabay-api";
+import createMarkup from "./js/render-functions";
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const form = document.querySelector(".form-search");
+const gallery = document.querySelector(".js-gallery");
+const loader = document.querySelector(".loader");
+let lightbox;
+
+form.addEventListener("submit", querySearch);
+
+function querySearch(event) {
+    event.preventDefault();
+    loader.style.display = "block";
+
+    const inputVal = event.target.elements.query;
+   
+    fetchData(inputVal.value)
+        .then((response) => {
+            if (response.hits.length === 0) {
+                iziToast.show({
+            title: "Error",
+            message:  " Sorry, there are no images matching your search query. Please try again!",
+            position: "topRight",
+            color: "red"
+                })
+            }
+            
+            const markup = createMarkup(response.hits);
+            gallery.innerHTML = markup;
+            loader.style.display = "none";
+            
+            if (lightbox) {
+                lightbox.refresh();
+            } else {
+                lightbox = new SimpleLightbox(".gallery a"); 
+            }
+            
+            console.log(response.hits.length);
+        })
+        .catch(error => {
+            console.log(error);
+            
+            iziToast.show({
+                title: "Error",
+                message:  " Sorry, there are no images matching your search query. Please try again!",
+                position: "topRight",
+                color: "red"
+            })
+        })
+}
